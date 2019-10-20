f64891c33b9defc24abe71ae4013213e341fb29c
yuhuih052 <48513815+yuhuih052@users.noreply.github.com>
2019-10-20 20:44:33 +0800

    基础页面结构

---
+++ larabbs/resources/views/layouts/_footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="footer">
+ <div class="container">
+ <p class="float-left">
+ 由 <a href="https://weibo.com/5812722370/profile?rightmod=1&wvr=6&mod=personinfo&is_all=1" target="_blank">Yuhui</a> 设计和编码 <span class="caret"></span>
+
+ </p>
+ <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
+ </div>
+</footer>
