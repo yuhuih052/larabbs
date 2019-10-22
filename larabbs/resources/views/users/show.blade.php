cfd1212c9a639b6366538973c52a6c5d00c37f5e
yuhuih052 <48513815+yuhuih052@users.noreply.github.com>
2019-10-22 15:34:33 +0800

    编辑个人资料

--- larabbs/resources/views/users/show.blade.php
+++ larabbs/resources/views/users/show.blade.php
@@ -4,7 +4,7 @@
 <div class="row">
 	<div class="col-lg-3 col-md-3 hidden-sm hidden-xs user-info">
 		<div class="card ">
-			<img class="card-img-top" src="">
+			<img class="card-img-top" src="#">
 			<div class="card-body">
 				<h5>
 					<strong>个人简介</strong>
