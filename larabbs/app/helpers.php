8116ed23ec7d8d36bc8885aa7fbf5f99567ab1e8
vagrant <vagrant@homestead>
2019-10-23 11:14:58 +0000

    分类话题列表

--- larabbs/app/helpers.php
+++ larabbs/app/helpers.php
@@ -4,3 +4,7 @@ function route_class()
 {
 	return str_replace('.','_', Route::currentRouteName());
 }
+function category_nav_active($category_id)
+{
+ return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
