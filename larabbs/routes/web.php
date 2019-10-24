6503bfacccc7163b8a1514b75977146230c178b3
vagrant <vagrant@homestead>
2019-10-24 06:46:34 +0000

    用户可以上传话题图片

--- larabbs/routes/web.php
+++ larabbs/routes/web.php
@@ -35,3 +35,4 @@ Route::get('email/resend', 'Auth\VerificationController@resend')->name('verifica
 Route::resource('users','UsersController',['only' => ['show','update','edit']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');
