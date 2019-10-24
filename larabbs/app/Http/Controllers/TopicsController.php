e3940fce1431b0b0df467d2c580f2cc14d623050
vagrant <vagrant@homestead>
2019-10-24 04:08:52 +0000

    用户可以创建话题

--- larabbs/app/Http/Controllers/TopicsController.php
+++ larabbs/app/Http/Controllers/TopicsController.php
@@ -6,6 +6,8 @@ use App\Models\Topic;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
+use App\Models\Category;
+use Auth;
 
 class TopicsController extends Controller
 {
@@ -14,7 +16,7 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index((Request $request, Topic $topic)
+	public function index(Request $request, Topic $topic)
 	{
 		$topics = $topic->withOrder($request->order)->paginate(10);
 		return view('topics.index', compact('topics'));
@@ -27,13 +29,16 @@ class TopicsController extends Controller
 
 	public function create(Topic $topic)
 	{
-		return view('topics.create_and_edit', compact('topic'));
+		$categories = Category::all();
+		return view('topics.create_and_edit', compact('topic','categories'));
 	}
 
-	public function store(TopicRequest $request)
+	public function store(TopicRequest $request, Topic $topic)
 	{
-		$topic = Topic::create($request->all());
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+		$topic->fill($request->all());
+		$topic->user_id = Auth::id();
+		$topic->save();
+		return redirect()->route('topics.show', $topic->id)->with('message', '帖子创建成功');
 	}
 
 	public function edit(Topic $topic)
--- larabbs/resources/views/layouts/_header.blade.php
+++ larabbs/resources/views/layouts/_header.blade.php
@@ -23,17 +23,29 @@
 	 			<li class="nav-item"><a class="nav-link" href="{{ route('login')}}">登录</a></li>
 	 			<li class="nav-item"><a class="nav-link" href="{{ route('register')}}">注册</a></li>
 	 			@else
+				<li class="nav-item">
+					<a class="nav-link mt-1 mr-3 font-weight-bold" href="{{ route('topics.create') }}">
+					 	<i class="fa fa-plus"></i>
+					</a>
+				 </li>
+
 	 			<li class="nav-item dropdown">
 	 				<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">
 	 					<img src="{{ Auth::user()->avatar}}" class="img-responsive img-circle" width="30px" height="30px">
 	 					{{ Auth::user()->name }}
 					 </a>
 	 				<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-		 				<a class="dropdown-item" href="{{route('users.show',Auth::id())}}">个人中心</a>
-		 				<a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">编辑资料</a>
+		 				<a class="dropdown-item" href="{{route('users.show',Auth::id())}}">
+		 					<i class="far fa-user mr-2"></i>
+		 					个人中心
+		 				</a>
+		 				<a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">
+		 					<i class="far fa-edit mr-2"></i>
+		 					编辑资料
+		 				</a>
 		 				<div class="dropdown-divider"></div>
 			 			<a class="dropdown-item" id="logout" href="#">
-			 					<form action="{{ route('logout') }}" method="POST">
+			 					<form action="{{ route('logout') }}" method="POST" onsubmit="return confirm('您确定要退出吗')">
 								 	{{ csrf_field() }}
 									 <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
 			 					</form>
