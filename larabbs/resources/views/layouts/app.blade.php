5c6665d8060c0cf58f9c157ce69a99cfe3cbb571
vagrant <vagrant@homestead>
2019-10-24 04:53:21 +0000

    WYSIWYG 编辑器

--- larabbs/resources/views/layouts/app.blade.php
+++ larabbs/resources/views/layouts/app.blade.php
@@ -4,42 +4,45 @@
 
 <head>
 
- <meta charset="utf-8">
+	 <meta charset="utf-8">
 
- <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
- <meta name="viewport" content="width=device-width, initial-scale=1">
+	 <meta name="viewport" content="width=device-width, initial-scale=1">
 
- <!-- CSRF Token -->
- <meta name="csrf-token" content="{{ csrf_token() }}">
+	 <!-- CSRF Token -->
+	 <meta name="csrf-token" content="{{ csrf_token() }}">
 
- <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
+	 <title>@yield('title', 'LaraBBS') - Laravel 进阶教程</title>
 
- <!-- Styles -->
- <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+	 <!-- Styles -->
+	 <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+	 @yield('styles')
 
 </head>
 <body>
 
- <div id="app" class="{{ route_class() }}-page">
+	 <div id="app" class="{{ route_class() }}-page">
 
- @include('layouts._header')
+	 @include('layouts._header')
 
- <div class="container">
+	 <div class="container">
 
- @include('shared._messages')
+	 @include('shared._messages')
 
- @yield('content')
+	 @yield('content')
 
- </div>
+	 </div>
 
- @include('layouts._footer')
+	 @include('layouts._footer')
 
- </div>
+	 </div>
 
- <!-- Scripts -->
+	 <!-- Scripts -->
 
- <script src="{{ mix('js/app.js') }}"></script>
+	 <script src="{{ mix('js/app.js') }}"></script>
+	 
+	 @yield('scripts')
 
 </body>
 
