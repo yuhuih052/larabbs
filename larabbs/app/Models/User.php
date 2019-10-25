10de7b4e95428a0b1e6be156bcf03103ed5aaa2a
vagrant <vagrant@homestead>
2019-10-25 02:01:57 +0000

    回复数据

--- larabbs/app/Models/User.php
+++ larabbs/app/Models/User.php
@@ -49,5 +49,10 @@ class User extends Authenticatable implements MustVerifyEmailContract
         return $this->id == $model->user_id;
     }
 
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
+
 
 }
--- larabbs/database/seeds/DatabaseSeeder.php
+++ larabbs/database/seeds/DatabaseSeeder.php
@@ -11,7 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
         $this->call(UsersTableSeeder::class);
-		$this->call(TopicsTableSeeder::class);
+        $this->call(TopicsTableSeeder::class);
+		$this->call(ReplysTableSeeder::class);
+
     }
 }
