fcca9681634353424558cef775ed1a4340f59d89
vagrant <vagrant@homestead>
2019-10-28 05:17:56 +0000

    侧边栏链接

--- larabbs/app/Providers/AppServiceProvider.php
+++ larabbs/app/Providers/AppServiceProvider.php
@@ -24,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
 		\App\Models\User::observe(\App\Observers\UserObserver::class);
 		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+        \App\Models\Link::observe(\App\Observers\LinkObserver::class);
 
         //修改Carbon对象为中文
         \Carbon\Carbon::setLocale('zh');
--- larabbs/database/seeds/DatabaseSeeder.php
+++ larabbs/database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(TopicsTableSeeder::class);
 		$this->call(ReplysTableSeeder::class);
+        $this->call(LinksTableSeeder::class);
 
     }
 }
