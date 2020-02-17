9c022abf42c5dcaa850e42cd556e06c3d6ac9904
vagrant <vagrant@homestead>
2020-02-17 07:45:39 +0000

    判斷是否綫上環境

---
+++ larabbs/app/Http/Requests/Api/UserRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+
+    public function authorize()
+    {
+        return true;
+    }
+
+
+    public function rules()
+    {
+        return [
+
+        ];
+    }
+}
